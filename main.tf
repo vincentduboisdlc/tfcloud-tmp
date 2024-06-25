a08ea427b262056dbcfeb0959dc9db54df73a350
vincent <vincent@0047697.province-sud.nc>
2024-06-25 13:08:25 +1100

    init

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      version = "5.35.0"
+    }
+  }
+  required_version = ">= 1.8"
+}
+
+variable "project_id" {
+  type        = string
+  description = "GCP project ID."
+  nullable    = false
+}
+
+variable "region" {
+  type        = string
+  description = "GCP project region."
+  nullable    = false
+  default     = "us-west1"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+data "google_container_cluster" "my_cluster" {
+  name     = "main"
+  location = "us-west1-a"
+}
+
+output "node_pool_name" {
+  value = data.google_container_cluster.my_cluster.node_pool[0].name
+}
